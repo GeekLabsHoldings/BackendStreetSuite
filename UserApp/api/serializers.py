6b32fc4c6cd11f162e1a02a8c7356b516afee42d
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-05-26 01:55:31 +0300

    adding BlogApp and CRUD operations for user details

--- UserApp/api/serializers.py
+++ UserApp/api/serializers.py
@@ -23,7 +23,7 @@ class UserSerializer(serializers.ModelSerializer):
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name) 
 
         
         password = validated_data.get('password')
