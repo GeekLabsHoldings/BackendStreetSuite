90ddba446ef8a8ea60381f65592b3a1e2bfb3643
Asem Yousry <asemgeeklabs@gmail.com>
2024-06-26 11:40:50 +0300

    edit on the change log for features

--- change_log/admin.py
+++ change_log/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import ChangeLog
+from .models import ChangeLog , Message
 # Register your models here.
 admin.site.register(ChangeLog)
+admin.site.register(Message)
--- change_log/api/urls.py
+++ change_log/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostChangeLog , ListChangeLog
+from .views import PostChangeLog , ListChangeLog , PostMessage , ListMessage
 
 urlpatterns =[
     path('post/',PostChangeLog.as_view()),
     path('list_all/',ListChangeLog.as_view()),
+    path('post_feature/',PostMessage.as_view()),
+    path('list_feature/',ListMessage.as_view()),
 ]
