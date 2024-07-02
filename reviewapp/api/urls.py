5fd18debbef106cbacc66e529d8fae51a1a0f56a
GLabs-Ahmed <ahmedtahageeklab@gmail.com>
2024-07-02 10:05:39 +0300

    merging

--- reviewapp/api/urls.py
+++ reviewapp/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReviewList, AddReview
+from .views import ReviewList, AddReview, UpdateReview
 
 
 urlpatterns = [
     path('get_reviews/',ReviewList.as_view(), name='_getreviews'),
     path('create_review/',AddReview.as_view(), name='create_review'),
+    path('update_review/<int:id>',UpdateReview.as_view(), name='update_review'),
 ]
