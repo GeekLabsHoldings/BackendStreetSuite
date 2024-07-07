14790fbc12534be19e994b8a5a02fb414b5bd464
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-07-07 11:15:46 +0300

    pushing to front

--- UserApp/admin.py
+++ UserApp/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 from.models import Profile , EmailVerification
 # Register your models here.
-
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+     list_display = ('user' , )
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(EmailVerification)
