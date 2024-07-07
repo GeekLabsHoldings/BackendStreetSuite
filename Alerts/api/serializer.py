80439eb68200e62f0dd99cba6deae40067bd983e
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-07-07 10:32:08 +0300

    pulling

--- Alerts/api/serializer.py
+++ Alerts/api/serializer.py
@@ -4,5 +4,6 @@ from rest_framework import serializers
 class RSISerializer(serializers.Serializer):
     message = serializers.CharField()
     ticker = serializers.CharField()
-    RSI = serializers.FloatField()
+    RSI = serializers.FloatField(required=False)
+    EMA = serializers.FloatField(required = False)
     risk_level = serializers.CharField()
