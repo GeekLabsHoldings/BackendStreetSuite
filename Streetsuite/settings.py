1cf0b7ba4a84facd025d5e679423aff502bd0adc
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-07-28 14:18:08 +0300

    finishing results on RSI

--- Streetsuite/settings.py
+++ Streetsuite/settings.py
@@ -269,7 +269,7 @@ CELERY_TIMEZONE = 'UTC'
 CELERY_BEAT_SCHEDULE = {
     'rsi-every-1-day': {
         'task': 'Alerts.tasks.RSI_1day',
-        'schedule': crontab(minute=0, hour=0),
+    #'schedule': crontab(minute=0, hour=0),
         "schedule":10 
     },
     'rsi-every-4-hours': {
