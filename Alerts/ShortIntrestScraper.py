66ec4924c89194e31b1024bfc81f33dbc0741d2c
Asem Yousry <asemgeeklabs@gmail.com>
2024-07-30 10:57:21 +0300

    add another scraping method for short interest

--- Alerts/ShortIntrestScraper.py
+++ Alerts/ShortIntrestScraper.py
@@ -28,6 +28,7 @@ def main(tickers):
     for i in range(len(tickers)):
         print(tickers[i])
         driver.get(f"https://www.benzinga.com/quote/{tickers[i]}/short-interest")
+
         # print("scraping", tickers[i])
         try:
             WebDriverWait(driver, 15).until(EC.presence_of_element_located(
@@ -50,4 +51,42 @@ def main(tickers):
         except:
             continue
         # print(value_dict)
+    return value_dict
+
+# scraping method for short interest value ##
+def short_interest_scraper(ticker_symvol):
+    print(ticker_symvol)
+    ## initialize webdriver ##
+    driver = webdriver.Chrome()
+    sleep(5)
+    ## open url on the web driver ##
+    driver.get(f'https://www.benzinga.com/quote/{ticker_symvol}/short-interest')
+    sleep(5)
+    value_dict = {}
+    ## check if advertisement is exists or not ##
+    try :
+        close_button = driver.find_element(By.XPATH,'//div[@class="CloseButton__ButtonElement-sc-79mh24-0 gkmgjx basslake-CloseButton basslake-close basslake-ClosePosition--top-right"]')
+        print('found x')
+        close_button.click()
+        print("clicked")
+        sleep(5)
+    except:
+        ...
+    finally:
+        ## get element of value of short interest ##
+        value = driver.find_elements(By.XPATH, '//div[@class="card-value font-extrabold"]')
+        sleep(3)
+        print("found element")
+        try:
+            value_text = value[1].text
+            sleep(2)
+            print(value_text)
+            value_string = value_text.strip("%")
+            float_value = float(value_string)
+            print(type(float_value))
+            if float_value >= 30:
+                # print("done")
+                value_dict[ticker_symvol] = value[1].text
+        except:
+            print("nothing")
     return value_dict
