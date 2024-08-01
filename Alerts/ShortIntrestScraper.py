6eae563d5effc6c641a0d800399dadde97e509af
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-08-01 17:03:27 +0300

    finalizing docker v3

--- Alerts/ShortIntrestScraper.py
+++ Alerts/ShortIntrestScraper.py
@@ -66,7 +66,7 @@ def short_interest_scraper(ticker_symvol):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-extensions")
     options.add_argument("disable-infobars")
-    chromedriver_path = '/usr/local/bin/chromedriver-linux64'
+    chromedriver_path = '/usr/local/bin/chromedriver-linux64/chromedriver'
     service = Service(executable_path=chromedriver_path)
     driver = webdriver.Chrome(service=service, options=options)
     
