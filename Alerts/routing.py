0d6d6138db4d3b2c6f01ad89fc911f4e6f22b316
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-08-22 09:47:28 +0300

    adding websocket on blogs

--- Alerts/routing.py
+++ Alerts/routing.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .consumers import WebSocketConsumer
+from BlogApp.consumers import BlogWSConsumer
 
 ws_urlpatterns = [
     path('ws/alerts/', WebSocketConsumer.as_asgi()),
+    path('ws/blogs/', BlogWSConsumer.as_asgi()),
 ]
