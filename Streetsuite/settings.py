be530ccba27ec1912dff7d1643e345ba5c835b0e
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-09-05 18:32:14 +0300

    testing twitter on AWS

--- Streetsuite/settings.py
+++ Streetsuite/settings.py
@@ -321,7 +321,7 @@ CELERY_BEAT_SCHEDULE = {
     'webscraper': 
     {
         'task': 'Alerts.tasks.twitter_scrap',
-        'schedule': crontab(minute=12, hour=15, day_of_month=5),
+        'schedule': crontab(minute=30, hour=15, day_of_month=5),
         # "schedule":20 
     },
     'Earning-15-days': {
