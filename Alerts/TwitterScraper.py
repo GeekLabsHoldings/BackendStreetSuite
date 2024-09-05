91d909bb9a06d6657ca1ed25d428b562bd0cabf6
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-09-05 18:48:53 +0300

    testing twitter on AWS

--- Alerts/TwitterScraper.py
+++ Alerts/TwitterScraper.py
@@ -45,36 +45,36 @@ def login():
     driver.get("https://x.com/i/flow/login")
     ######
     username_input = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//input[@class='r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7']")))
-    print(f"found username {username_input}")
+    print(f"found username {username_input.text}")
     username_input.send_keys('ahmedgeeklabs')
     ## click next button ##
     next_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//button[@class='css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-ywje51 r-184id4b r-13qz1uu r-2yi16 r-1qi8awa r-3pj75a r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l']")))
-    print(f"next button{next_button}")
+    EC.element_to_be_clickable((By.XPATH, "//button[@class='css-146c3p1 r-bcqeeo r-qvutc0 r-37j5jr r-q4m81j r-a023e6 r-rjixqe r-b88u0q r-1awozwy r-6koalj r-18u37iz r-16y2uox r-1777fci']")))
+    print(f"next button{next_button.text}")
     next_button.click()
     print("successfully!")
     ## add email ##
     try:
         email_input = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//input[@class='r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7']")))
-        print(f"email {email_input}")
+        print(f"email {email_input.text}")
         email_input.send_keys('ahmedtahageeklab@gmail.com')
         ## click next button ##
         next_button2 =  WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-19yznuf r-64el8z r-1fkl15p r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l']")))
-        print(f"next button{next_button2}")
+        print(f"next button{next_button2.text}")
         next_button2.click()
         print("successfully 22!")
     ####
     finally:
         password = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//input[@class='r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7']")))
         password.send_keys('Polo_1991')
-        print(f"found password {password}")
-        ## click log in button ##
+        print(f"found password {password.text}")
+        ## click log in button ##                                                                               css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-19yznuf r-64el8z r-1fkl15p r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l
         login_button =  WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-19yznuf r-64el8z r-1fkl15p r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l']")))
         login_button.click()
         print(f"successfully!!{login_button.text}")
         try:
             email_input = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//input[@class='r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7']")))
-            print(f"found email second one{email_input}")
+            print(f"found email second one{email_input.text}")
             email_input.send_keys('ahmedtahageeklab@gmail.com')
             ## click next button ##
             next_button2 =  WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-19yznuf r-64el8z r-1fkl15p r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l']")))
