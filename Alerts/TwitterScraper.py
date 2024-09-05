fe18ddff20ff9ee543711a3d55253198acc18acd
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-09-05 17:54:00 +0300

    testing twitter on AWS

--- Alerts/TwitterScraper.py
+++ Alerts/TwitterScraper.py
@@ -63,7 +63,7 @@ def login():
     ####
     finally:
         password = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//input[@class='r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7']")))
-        password.send_keys('ASMB2011asmb#')
+        password.send_keys('ASMB2011asmb$')
         print("found password")
         ## click log in button ##
         login_button =  WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-19yznuf r-64el8z r-1fkl15p r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l']")))
