edfa10b41dde7b2ab15c91907fa0f1dfc628339a
Asem Yousry <asemgeeklabs@gmail.com>
2024-09-19 14:00:49 +0300

    new

--- Streetsuite/settings.py
+++ Streetsuite/settings.py
@@ -306,7 +306,7 @@ CELERY_TIMEZONE = 'UTC'
 CELERY_BEAT_SCHEDULE = {
     'tasks-1-day': {
         'task': 'Alerts.tasks.tasks_1day',
-        'schedule': crontab(minute=51, hour=10),
+        'schedule': crontab(minute=4, hour=11),
         # "schedule":10 
     },
     'tasks-1-hour': {
