a3fbaffd353ca7fbf461200981c97adec246fee8
Asem Yousry <asemgeeklabs@gmail.com>
2024-09-19 09:27:23 +0300

    test

--- Streetsuite/settings.py
+++ Streetsuite/settings.py
@@ -332,7 +332,7 @@ CELERY_BEAT_SCHEDULE = {
     'webscraper': 
     {
         'task': 'Alerts.tasks.twitter_scrap',
-        'schedule': crontab(minute=35, hour=14, day_of_month=18),
+        'schedule': crontab(minute=35, hour=6, day_of_month=19),
         # "schedule":20 
     },
     'Earning-15-days': {
