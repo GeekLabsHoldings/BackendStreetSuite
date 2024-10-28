6f83aa369612eee1566205952bd347cdc87428a9
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-10-28 14:37:50 +0300

    removing testing stripe endpoint

--- Payment/api/urls.py
+++ Payment/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ProductPageView, CheckoutPageView, CancelationPageView, testingupgrade, WebHookView
+from .views import ProductPageView, CheckoutPageView, CancelationPageView, WebHookView
 
 urlpatterns = [
     path('', ProductPageView.as_view(), name="pricing"),
     path('checkout/<int:id>/', CheckoutPageView.as_view(), name='payment_intent'),
     path('webhook/', WebHookView, name='webhook'),
     path('cancel/', CancelationPageView.as_view(), name="cancel"),
-    path('test/', testingupgrade.as_view(), name='test')
 ]   
